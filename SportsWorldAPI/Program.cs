e3a16eb7379c1708ff99f054146bcd90ac849677
Alexander Toftesund <alexandertoftesund@gmail.com>
2025-12-03 13:46:23 +0100

    Created Data/ApplicationDbContext.cs

--- SportsWorldAPI/Program.cs
+++ SportsWorldAPI/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SportsWorldAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(connectionString));
 
 var app = builder.Build();
 
