2028a2cfa248e16554323c35c26c1795b1f96be1
Kennephh <150194783+Kennephh@users.noreply.github.com>
2025-12-03 16:41:12 +0100

    Lagt til et Interface av Venue, en mal på hvordan et Venue skal se ut

--- SportsWorldAPI/Interfaces/IVenue.cs
+++ SportsWorldAPI/Interfaces/IVenue.cs
@@ -3,7 +3,7 @@ namespace SportsWorldAPI.Interfaces;
 interface IVenue
 {
     int Id {get; set;}
-    string Name {get; set;}
+    string? Name {get; set;}
     int Capacity {get; set;}
-    string Image {get; set;}
+    string? Image {get; set;}
 }
