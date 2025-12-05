1aae5e9e17ea9c07c9e43bed6648e4dcef6c1ea9
Alexander Toftesund <alexandertoftesund@gmail.com>
2025-12-05 21:11:12 +0100

    Updated Program.cs (Added CORS policy)

--- SportsWorldAPI/Program.cs
+++ SportsWorldAPI/Program.cs
@@ -3,9 +3,25 @@ using SportsWorldAPI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var CorsPolicy = "_corsPolicy";
+
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: CorsPolicy,
+    policy =>
+    {
+        policy
+            .AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+
+    });
+});
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -27,6 +43,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors(CorsPolicy);
 
 app.MapControllers();
 
