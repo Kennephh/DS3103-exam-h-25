1adbb40b67c3138b77706805d64c31fc30c061b1
Kennephh <150194783+Kennephh@users.noreply.github.com>
2025-12-05 19:45:06 +0100

    Added a response interface for clearer feedback to users or other developers. Can also add more interfaces in the same file for other services

--- sports-world/src/services/VenueService.ts
+++ sports-world/src/services/VenueService.ts
@@ -6,8 +6,9 @@ const endpoint = "http://localhost:5103/api/venue";
 const getAllVenues = async () : Promise<IVenue[]> => {
     try {
         const response = await axios.get(endpoint);
-
+        return response.data;
     } catch (error) {
-        
+        console.log(error);
+        return [];
     }
 }
