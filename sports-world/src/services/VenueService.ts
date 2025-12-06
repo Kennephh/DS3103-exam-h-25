a996d6efe138d8cb8391bda1ea9b45f842731cb4
Kennephh <150194783+Kennephh@users.noreply.github.com>
2025-12-06 20:18:18 +0100

    Gjort endringer i VenueController slik at GetVenueByName returnerer en liste isteden for 1 venue. VenueService har fått getVenueByName

--- sports-world/src/services/VenueService.ts
+++ sports-world/src/services/VenueService.ts
@@ -1,14 +1,55 @@
 import axios from "axios";
-import { type IVenue } from "../interfaces/IVenue";
+import { type IVenue, } from "../interfaces/IVenue";
+import {type IVenueResponse } from "../interfaces/ResponseInterfaces";
 
 const endpoint = "http://localhost:5103/api/venue";
 
-const getAllVenues = async () : Promise<IVenue[]> => {
+const getAllVenues = async () : Promise<IVenueResponse> => {
     try {
-        const response = await axios.get(endpoint);
-        return response.data;
+        const response = await axios.get<IVenue[]>(endpoint);
+        return {
+            success: true,
+            data: response.data
+        }
     } catch (error) {
-        console.log(error);
-        return [];
+        console.error("Axios error fetching all venues: ", error);
+        return {
+            success: false,
+            data: null
+        }
     }
 }
+
+const getVenueById = async (id: number) : Promise<IVenueResponse> => {
+    try {
+        const response = await axios.get<IVenue>(endpoint + "/" + id)
+        return {
+            success: true,
+            data: response.data
+        }
+    } catch (error) {
+        console.error(`Axios error fetching venue with id: '${id}',` ,error);
+        return {
+            success: false,
+            data: null
+        }
+    }
+}
+
+const getVenueByName = async (name: string) : Promise<IVenueResponse> => {
+    try {
+        const response = await axios.get<IVenue[]>(endpoint + "/ByName/" + name)
+        return {
+            success: true,
+            data: response.data
+        }
+    } catch (error) {
+        console.error(`Axios error fetching venue with name(s): '${name}',` ,error);
+        return{
+            success: false,
+            data: null
+        }
+    }
+}
+
+export default {getAllVenues, getVenueById, getVenueByName, }
