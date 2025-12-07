4d2f44198bf9678166d36caf60cb1c6fe9657756
Alexander Toftesund <alexandertoftesund@gmail.com>
2025-12-07 08:43:09 +0100

    Added getAthleteById (+ visual tweaks)

--- sports-world/src/components/AthleteItem.tsx
+++ sports-world/src/components/AthleteItem.tsx
@@ -4,13 +4,23 @@ import { API_BASE_URL } from "../config";
 
 const AthleteItem: FC<{ athlete: IAthlete }> = ({ athlete }) => {
 
+    const buttonClasses = `
+        bg-sky-600
+        text-white
+        px-3
+        py-1
+        rounded
+        hover:bg-sky-700
+        hover:cursor-pointer
+    `;
+
     return (
         <article className="
             h-full
             w-full
             p-4
             bg-white
-            rounded-lg
+            rounded
             shadow-xl
             hover:cursor-pointer
             hover:scale-102
@@ -18,36 +28,20 @@ const AthleteItem: FC<{ athlete: IAthlete }> = ({ athlete }) => {
             ">
 
             {athlete.image && (
-                <img src={API_BASE_URL + athlete.image} alt={athlete.name} className="w-full aspect-3/4 object-cover rounded-lg mb-4" />
+                <img src={API_BASE_URL + athlete.image} alt={athlete.name} className="w-full aspect-3/4 object-cover rounded mb-4 border-2 border-gray-700" />
             )}
 
             <h3 className="text-xl font-bold mb-2">{athlete.name}</h3>
             <p className=""><span className="font-semibold">Gender:</span> {athlete.gender}</p>
-            <p className=""><span className="font-semibold">Price:</span> {athlete.price}</p>
+            <p className=""><span className="font-semibold">Price:</span> {"$" + athlete.price + " (USD)"}</p>
             <p className="">
                 <span className="font-semibold">Status:</span>
                 <span className={athlete.purchaseStatus ? "" : "text-red-600"}> { athlete.purchaseStatus ? "Purchased" : "Not purchased" }</span>
             </p>
 
              <div className="mt-4 flex justify-end gap-2">
-                    <button className="
-                        bg-sky-600
-                        text-white
-                        px-3
-                        py-1
-                        rounded
-                        hover:bg-sky-700
-                        ">Edit
-                    </button>
-                    <button className="
-                        bg-sky-600
-                        text-white
-                        px-3
-                        py-1
-                        rounded
-                        hover:bg-sky-700
-                        ">Delete
-                    </button>
+                <button className={buttonClasses}>Edit</button>
+                <button className={buttonClasses}>Delete</button>
             </div>
 
         </article>
--- sports-world/src/components/layout/AppLayout.tsx
+++ sports-world/src/components/layout/AppLayout.tsx
@@ -2,24 +2,26 @@
 import { Link, Outlet } from "react-router-dom";
 
 const AppLayout = () => {
-    return (
-        <div className="min-h-screen bg-gray-100">
-            <nav className="bg-sky-600 p-4 text-white shadow-md">
-                <div className="container mx-auto flex justify-between">
-                    <Link to="/" className="font-bold text-xl">SportsWorld</Link>
-                    <ul className="flex gap-4">
-                      <li><Link to="/athletes" className="hover:underline">Athletes</Link></li>
-                      <li><Link to="/finance" className="hover:underline">Finance</Link></li>
-                      <li><Link to="/venues" className="hover:underline">Venues</Link></li>
-                    </ul>
-                </div>
-            </nav>
+  return (
+      <div className="min-h-screen bg-gray-100">
 
-          <main className="container mx-auto p-4">
-            <Outlet />
-          </main>
-        </div>
-      )
+          <nav className="bg-sky-600 p-4 text-white shadow-md">
+              <div className="container mx-auto flex justify-between items-center">
+                  <Link to="/" className="text-xl font-bold">SportsWorld</Link>
+                  <ul className="flex gap-4">
+                    <li><Link to="/athletes" className="hover:underline">Athletes</Link></li>
+                    <li><Link to="/finance" className="hover:underline">Finance</Link></li>
+                    <li><Link to="/venues" className="hover:underline">Venues</Link></li>
+                  </ul>
+              </div>
+          </nav>
+
+        <main className="container mx-auto p-4">
+          <Outlet />
+        </main>
+
+      </div>
+    )
 }
 
 export default AppLayout
