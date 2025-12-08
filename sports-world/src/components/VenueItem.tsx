d6b0f99b3135734bda43593b05e5f25c72a63153
Kennephh <150194783+Kennephh@users.noreply.github.com>
2025-12-08 18:55:47 +0100

    Laget VenueItem, VenueList og begynt på VenuesPage. Lagt inn venue bilder og oppdatert databasen

--- sports-world/src/components/VenueItem.tsx
+++ sports-world/src/components/VenueItem.tsx
@@ -1,10 +1,35 @@
 import {type IVenue } from "../interfaces/IVenue";
+import { API_BASE_URL } from "../config";
 
 export interface IVenueItemProps{
     venue: IVenue
 }
 
 const VenueItem = ({venue}: IVenueItemProps) => {
+
+    return(
+        <article className="
+            h-full
+            w-full
+            p-4
+            bg-white
+            rounded
+            shadow-xl
+            hover:cursor-pointer
+            hover:scale-102
+            transition-all
+        ">
+            <h2>
+                {venue.name}
+            </h2>
+            <div className="h-48 w-full bg-gray-200 rounded-md mb-4 overflow-hidden">
+                <img src={API_BASE_URL + venue.image} alt={venue.name} />
+            </div>
+            <p>
+                Capacity: {venue.capacity}
+            </p>
+        </article>
+    );
 
 }
 
