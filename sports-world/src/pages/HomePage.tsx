fcac6985b7272f1c0b46de18e25253ac14500263
Alexander Toftesund <alexandertoftesund@gmail.com>
2025-12-09 15:50:36 +0100

    Update to homepage

--- sports-world/src/pages/HomePage.tsx
+++ sports-world/src/pages/HomePage.tsx
@@ -1,13 +1,13 @@
 import { Link } from "react-router-dom"
-import arena1 from "../assets/arena1.jpg"
-import fighter1 from "../assets/fighter1.jpg"
-import venue1 from "../assets/venue1.jpg"
+import athlete from "../assets/athlete.webp"
+import finance from "../assets/finance.webp"
+import venue from "../assets/venue.webp"
 
 const HomePage = () => {
 
     const linkClassNames = `
         p-4
-        bg-black/40
+        bg-black/60
         text-white
         rounded
         hover:bg-black/10
@@ -17,9 +17,9 @@ const HomePage = () => {
         text-4xl
         font-semibold
         w-full h-full
-        drop-shadow-lg
         tracking-wider
         transition-all
+        hover:scale-115
     `;
 
     const linkDivClassNames = `
@@ -27,40 +27,38 @@ const HomePage = () => {
         flex
         flex-col
         rounded
-        border-2 border-black/75
         overflow-hidden
         hover:scale-105
-        hover:border-sky-700
         transition-all
         hover:shadow-xl
     `;
 
     const bgStyle1 = {
-        backgroundImage: `url(${arena1})`,
+        backgroundImage: `url(${athlete})`,
         backgroundSize: "cover",
         backgroundPosition: "center"
     };
     const bgStyle2 = {
-        backgroundImage: `url(${fighter1})`,
+        backgroundImage: `url(${finance})`,
         backgroundSize: "cover",
         backgroundPosition: "center"
     };
     const bgStyle3 = {
-        backgroundImage: `url(${venue1})`,
+        backgroundImage: `url(${venue})`,
         backgroundSize: "cover",
         backgroundPosition: "center"
     };
 
     return(
-        <div className="container mx-auto h-[calc(100vh-4rem)] flex flex-col gap-4 p-4">
+        <div className="container mx-auto h-[calc(100vh-4rem)] flex flex-col gap-4 py-4">
             <div className={linkDivClassNames} style={bgStyle1}>
-                <Link to="/athletes" className={linkClassNames}>🥋 Athletes</Link>
+                <Link to="/athletes" className={linkClassNames}>Athletes</Link>
             </div>
             <div className={linkDivClassNames} style={bgStyle2}>
-                <Link to="/finance" className={linkClassNames}>💰 Finance</Link>
+                <Link to="/finance" className={linkClassNames}>Finance</Link>
             </div>
             <div className={linkDivClassNames} style={bgStyle3}>
-                <Link to="/venues" className={linkClassNames}>🏟️ Venues</Link>
+                <Link to="/venues" className={linkClassNames}>Venues</Link>
             </div>
         </div>
     )
