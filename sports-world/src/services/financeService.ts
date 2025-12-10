3bdd1c1795e3c38b8801979953af06159b8303af
Michael Sasen Davidsen <md.vidsen@gmail.com>
2025-12-10 14:51:16 +0100

    Update

--- sports-world/src/services/financeService.ts
+++ sports-world/src/services/financeService.ts
@@ -1,14 +1,55 @@
 import axios from "axios";
 import {type IFinance} from "../interfaces/IFinance";
 import { API_PATHS } from "../config";
+import {type IFinanceResponse} from "../interfaces/ResponseInterfaces";
+
 
 const endpoint = API_PATHS.FINANCE;
 
-const getFinance = async () : Promise<IFinance | undefined> => {
+const getFinance = async () : Promise<IFinanceResponse | undefined> => {
     try{
         const response = await axios.get<IFinance>(`${endpoint}`);
-        return response.data;
-    } catch{
+        return{
+            success : true,
+            data : response.data
+        }
+    } catch (error) {
+        console.error("Error fetching finances", error);
+        return {
+            success : false,
+            data : null
+        }
+    }
+}
 
+const postFinance = async (newFinance : IFinance) : Promise<IFinanceResponse | undefined> => {
+    try{
+        const response = await axios.post<IFinance>(endpoint, newFinance);
+        return{
+            success : true,
+            data : response.data
+        }
+    } catch (error) {
+        console.error("Error posting finances", error);
+        return {
+            success : false,
+            data : null
+        }
+    }
+}
+
+const putFinance = async (newFinance : IFinance) : Promise<IFinanceResponse | undefined> => {
+    try{
+        const response = await axios.put(endpoint, newFinance);
+        return{
+            success : true,
+            data : response.data
+        }
+    } catch (error) {
+        console.error("Error updating finances", error);
+        return {
+            success : false,
+            data : null
+        }
     }
 }
