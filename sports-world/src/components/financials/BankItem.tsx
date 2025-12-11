9553b90051fd4a9ef0a9741e36aeb08395de8267
Michael Sasen Davidsen <md.vidsen@gmail.com>
2025-12-11 15:14:18 +0100

    update

--- sports-world/src/components/financials/BankItem.tsx
+++ sports-world/src/components/financials/BankItem.tsx
@@ -1,3 +1,50 @@
 import { type IFinance } from "../../interfaces/IFinance";
 
-export interface
+export interface BankProps{
+    financials: IFinance
+}
+
+const BankItem = ({financials} : BankProps) => {
+
+    const buttonClasses = `
+        bg-sky-600
+        text-white
+        px-3
+        py-1
+        rounded
+        hover:bg-sky-700
+        hover:cursor-pointer
+    `;
+
+    return (
+            <article className="
+                h-full
+                w-full
+                bg-white
+                rounded
+                shadow-xl
+                hover:cursor-pointer
+                hover:scale-105
+                transition-all
+                overflow-hidden
+                ">
+    
+            
+                <div className="px-2 flex flex-col">
+                    <h3 className="text-lg font-semibold mb-2">SportBank</h3>
+                    <p>Debt: ?</p>
+                </div>
+                
+                <div className="m-2 flex justify-end gap-2">
+                    <input type="text"
+                    placeholder="Enter loan amount..." 
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700"
+                />
+                    <button className={buttonClasses}>Loan</button>
+                </div>
+    
+            </article>
+        );
+}
+
+export default BankItem;
---
+++ sports-world/src/pages/FinancePage.tsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+const FinancePage = () => {
+
+    return(
+        <>
+
+
+
+        </>
+    )
+} 
+
+export default FinancePage;
