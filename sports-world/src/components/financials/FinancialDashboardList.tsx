a0b19a3b57f7164784d1d9f35884c01649bbe39a
Michael Sasen Davidsen <md.vidsen@gmail.com>
2025-12-16 23:07:03 +0100

    Update FinancialDashboardList.tsx

--- sports-world/src/components/financials/FinancialDashboardList.tsx
+++ sports-world/src/components/financials/FinancialDashboardList.tsx
@@ -3,7 +3,6 @@ import BankItem from "./BankItem";
 
 const FinancialDashboardList: React.FC  = () => {
     return(
-        // DETTE SER KNOTETE UT PÅ TELEFON, 
         <section className="flex space-x-4">
             <div className="flex flex-col w-full gap-4 ">
                 <div className="flex-1">
